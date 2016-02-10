dcc194d6a6b596553abf5e5d786e4758260ea7df
Marcus Henry Ewert <user@marcushenryewert.com>
2016-02-09 16:03:51 -0800

    update test to use the new interface

--- bookshelf/tests/api_v3/test_interfaces.py
+++ bookshelf/tests/api_v3/test_interfaces.py
@@ -3,9 +3,9 @@ import unittest
 
 from zope.interface.verify import verifyObject, verifyClass
 
-from bookshelf.api_v3.gce import GCE
-from bookshelf.api_v3.ec2 import EC2
-from bookshelf.api_v3.rackspace import Rackspace
+from bookshelf.api_v3.gce import GCEInstance
+from bookshelf.api_v3.ec2 import EC2Instance
+from bookshelf.api_v3.rackspace import RackspaceInstance
 from bookshelf.api_v3.cloud_instance import (
     ICloudInstanceFactory,
     ICloudInstance
@@ -15,28 +15,28 @@ from bookshelf.api_v3.cloud_instance import (
 class TestGCEInterfaces(unittest.TestCase):
 
     def test_gce_provides_cloud_instance_factory(self):
-        verifyObject(ICloudInstanceFactory, GCE)
+        verifyObject(ICloudInstanceFactory, GCEInstance)
 
     def test_gce_implements_cloud_instance(self):
-        verifyClass(ICloudInstance, GCE)
+        verifyClass(ICloudInstance, GCEInstance)
 
 
 class TestEC2Interfaces(unittest.TestCase):
 
     def test_ec2_provides_cloud_instance_factory(self):
-        verifyObject(ICloudInstanceFactory, EC2)
+        verifyObject(ICloudInstanceFactory, EC2Instance)
 
     def test_ec2_implements_cloud_instance(self):
-        verifyClass(ICloudInstance, EC2)
+        verifyClass(ICloudInstance, EC2Instance)
 
 
 class TestRackspaceInterfaces(unittest.TestCase):
 
     def test_rackspace_provides_cloud_instance_factory(self):
-        verifyObject(ICloudInstanceFactory, Rackspace)
+        verifyObject(ICloudInstanceFactory, RackspaceInstance)
 
     def test_rackspaceee_implements_cloud_instance(self):
-        verifyClass(ICloudInstance, Rackspace)
+        verifyClass(ICloudInstance, RackspaceInstance)
 
 
 if __name__ == '__main__':
